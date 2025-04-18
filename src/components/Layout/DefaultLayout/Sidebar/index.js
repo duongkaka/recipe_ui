b5e9bd159caa4218d785891504e0ad4f55b614bf
duongkaka <duongkaka96@gmail,com>
2025-04-18 21:34:08 +0900

    dung khung layout

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,8 @@
-function Sidebar() {
-    return <h2>Sidebar</h2>;
+import styles from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+function Sidebar({ className }) {
+    return <aside className={cx('wrapper')}>Sidebar</aside>;
 }
 
 export default Sidebar;
